f47ca20d3999a67eaee93f550f132cb5bf3d77e3
Kunal Maloo <kunnalmaloo@gmail.com>
2024-04-03 00:54:17 +0530

    set headers for cors

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ server.use(bodyParser.urlencoded({ extended: true }));
 
 server.use(morgan('dev'));
 
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://organic-shop-frontend.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 server.use(cors({
   origin: 'https://organic-shop-frontend.vercel.app', // Replace with your front-end page URL
   credentials: true
